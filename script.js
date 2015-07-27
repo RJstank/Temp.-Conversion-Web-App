dc102b339b41e01436f7cd385b8a19dfafc93c91
RJstank <ryan_stan@comcast.net>
2015-07-26 20:24:31 -0400

    Finished Project

---
+++ script.js
@@ -0,0 +1,26 @@
+function processForm() {
+    
+    var temperature = Number(document.tempForm.temp.value);
+    var tempType;
+    var result;
+
+    for (var i=0; i < document.tempForm.choice.length; i++) {
+        
+        if (document.tempForm.choice[i].checked) {
+            tempType = document.tempForm.choice[i].value;
+        }
+    }
+
+    if (tempType == 'fahrenheit') {
+       var result = temperature * 9/5 + 32;
+    }
+    
+    else {
+       var result = (temperature -  32)  *  5/9;
+    }
+    
+    // Assign the result field value here
+    document.tempForm.resultField.value= result
+    
+    
+}
